1f34eb64c40bcac4d569a88f57696c74010773c6
Mohammad Pakdast Nobari <135954454+mr-mpn@users.noreply.github.com>
2026-01-04 14:08:21 +0100

    (feat) make api calls to the backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import { useState } from 'react'
 import './App.css'
-import Axios from 'axios'
+import axios from 'axios'
 
 function App() {
   
   const [value , setValue] = useState('')
-
+  const [imdbRating , setImdbRating] = useState(0)
   const inputHandler=(e)=>{
     setValue(e.target.value)
   }
 
   const searchButtonHandler = (e)=>{
     e.preventDefault() // Prevent form submission
-    alert(`The value that has been searched is ${value}`)
+    //alert(`The value that has been searched is ${value}`)
+    axios.get(`http://localhost:3000/dev/query-movies?title=${value}`)
+  .then(response => {
+    console.log(response.data); 
+    setImdbRating(response.data?.data?.movie?.imdbRating)
+  })
+  .catch(error => {
+    console.error('Error making the request:', error);
+  });
   }
   return (
     <>
@@ -22,7 +30,8 @@ function App() {
         <input type='text' onChange={inputHandler} placeholder='write the name of the movie' />
         <button type='submit' onClick={searchButtonHandler}>Search</button>
       </form> 
-      <p>the value is {value}</p>
+      <p>the name of the movie you searched is {value}</p>
+      <p>IMDB Rating {imdbRating}</p>
     </div>
     </>
   )
